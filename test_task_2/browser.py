44b0d7557f4e046af45480ee9e09e7c06f4537ba
Yana Shafar <shafaryana01@gmail.com>
2022-06-23 22:38:13 +0300

    Add test task

---
+++ test_task_2/browser.py
@@ -0,0 +1,13 @@
+from test_task_2.driver import Driver
+
+
+class Browser:
+
+    def __init__(self):
+        self.driver = Driver().get_driver()
+
+    def open_url(self, url):
+        self.driver.get(url)
+
+    def maximize_window(self):
+        self.driver.maximize_window()
--- test_task_2/pages/base_page.py
+++ test_task_2/pages/base_page.py
@@ -3,25 +3,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from test_task_2.driver import Driver
+
 
 class BasePage:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def _get_locator_by_type(self, locator):
-        if locator.startswith("//") or locator.startswith(".//") or locator.startswith("("):
-            return By.XPATH
-
-        return By.CSS_SELECTOR
+    def __init__(self):
+        self.driver = Driver().get_driver()
+        self.locator_type = By.XPATH
 
     def find_element(self, locator, time_out_sec=10):
         return self.wait_for_element_present(locator, time_out_sec)
 
     def wait_for_element_present(self, locator, time_out_sec=10):
-        locator_by_type = self._get_locator_by_type(locator)
 
         element = WebDriverWait(self.driver, time_out_sec).until(
-            EC.presence_of_element_located((locator_by_type, locator)))
+            EC.presence_of_element_located((self.locator_type, locator)))
 
         return element
 
@@ -48,14 +44,11 @@ class BasePage:
         element.send_keys(text)
 
     def scroll_to_element_view(self, locator):
-        by_type = self._get_locator_by_type(locator)
         self.wait_for_element_present(locator)
-        element = self.driver.find_element(by_type, locator)
+        element = self.driver.find_element(self.locator_type, locator)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
     def click_with_java_script(self, locator):
-        by_type = self._get_locator_by_type(locator)
         self.wait_for_element_present(locator)
-        element = self.driver.find_element(by_type, locator)
+        element = self.driver.find_element(self.locator_type, locator)
         self.driver.execute_script("arguments[0].click();", element)
-
